0572e9cd0a9e853392d8ffd0cdfef18553fdbda6
nyarmith <powah.serge@gmail.com>
2017-12-04 17:51:45 -0500

    correction added

--- tool_specific/clearcase.sh
+++ tool_specific/clearcase.sh
@@ -4,6 +4,6 @@ if type cleartool >/dev/null 2>/dev/null; then
         alias sv="$__ct setview -login"
         alias rmpriv='ct lspriv | grep -v "^/bce" | grep -v "/tags$" | grep -v "\[checkedout\]" | xargs rm -r'
     else
-        echo "* Clearcase Not found, add cleartool to your path and source init.ksh again"
+        echo "* Clearcase Not found, add cleartool to your path and source init.sh again"
 fi
 
