153d494c868563f19dd3c50d55585f1f009eb1e9
nyarmith <powah.serge@gmail.com>
2018-06-15 12:40:00 +0000

    new alias

--- aliases/basic_aliases.sh
+++ aliases/basic_aliases.sh
@@ -29,3 +29,6 @@ alias egrep='egrep --color=auto'
 alias python=python3
 
 alias t=tmux
+
+alias g++="g++ -std=c++14"
+alias dg++="g++ -g -Wall -Wextra -pedantic -std=c++14 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector"
