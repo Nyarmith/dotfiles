0fc281aa37e7dc03721398242abaef45d3ad26f6
Sergey Ivanov <powah.serge@gmail.com>
2019-07-06 00:27:40 -0400

    added readline escape for non-printable characters so it can keep track of cursor position

--- ps_config/ps1.sh
+++ ps_config/ps1.sh
@@ -12,8 +12,8 @@ __make_ps1(){
     local retStr=""
     local fmtStr=""
     if [[ -n $COL_ON ]]; then
-        (( "$prevRet" )) && retStr="${fgcol[red]}${prevRet}${COL_OFF} "
-        fmtStr="${fgcol[yellow]}\\\\u@\\\\h ${fgcol[white]}[${fgcol[magenta]}\\\\w${fgcol[white]}]${COL_OFF}%.12s %s"
+        (( "$prevRet" )) && retStr="\[${fgcol[red]}\]${prevRet}\[${COL_OFF}\] "
+        fmtStr="\[${fgcol[yellow]}\]\\\\u@\\\\h \[${fgcol[white]}\][\[${fgcol[magenta]}\]\\\\w\[${fgcol[white]}\]]\[${COL_OFF}\]%.12s %s"
     else
         (( "$prevRet" )) && retStr="${prevRet} "
         fmtStr="\\\\u@\\\\h [\\\\w]%.12s %s "
@@ -21,8 +21,11 @@ __make_ps1(){
     printf "${fmtStr}$ " "$branch" "$retStr"
 }
 
+# https://stackoverflow.com/questions/19092488/custom-bash-prompt-is-overwriting-itself
+
 PROMPT_COMMAND='PS1=$(__make_ps1)'
 export PS2='.. '
 
 #finally, make sure to trap interrupts
 trap 2 15
+
