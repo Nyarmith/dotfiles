410f8b53f40b9a689a0a3fbe832f04efc2592cc8
Sergey Ivanov <powah.serge@gmail.com>
2019-07-09 22:00:48 -0400

    fix newline in git module init

--- tool_ext/git.sh
+++ tool_ext/git.sh
@@ -1,15 +1,15 @@
 if type git >/dev/null 2>/dev/null; then
   initthisrepo(){
-    printf "! are you sure you want to repofy this directory? [Y/n]\n"
+    printf "! are you sure you want to repofy this directory? [y/n]\n"
 
     read ans
-    [[ "$ans" != 'Y' ]] && {
+    [[ "$ans" != 'y' ]] && {
         printf "aborting!\n"
         return 0
     }
 
     local repo="$(basename `pwd`)"
-    printf "creating github repository : ${repo}"
+    printf "creating github repository : ${repo}\n"
 
     curl -u 'nyarmith' https://api.github.com/user/repos -d "{\"name\":\"$repo\"}" || {
         printf "error creating repo, aborting!\n"
