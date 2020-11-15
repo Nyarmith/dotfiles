8e13191c85045a88c5d0ec66dee78f5c4f9ae208
nyarmith <powah.serge@gmail.com>
2020-11-15 12:22:44 -0500

    added messages to timer

--- misc/pomodoro.sh
+++ misc/pomodoro.sh
@@ -9,6 +9,11 @@ export FIFO="/tmp/pomodoro"
 
 __countdown()
 {
+    local message="none"
+    [ ! -z "$2" ] && {
+        message="$2"
+    }
+
     local START_SHORT="$SFXDIR/windup_short.wav"
     local START_LONG="$SFXDIR/windup_long.wav"
     local HALFWAY="$SFXDIR/ticking.wav"
@@ -28,7 +33,7 @@ __countdown()
         play "$START_SHORT" &> /dev/null
     fi
 
-    echo "$BASHPID $otime $time" > $FIFO
+    echo "$BASHPID $otime $time $message" > $FIFO
     while ((time > 0)); do
         if ((time > 15)); then
             sleep 15;
@@ -45,56 +50,83 @@ __countdown()
         echo "$BASHPID $otime $time" > $FIFO
     done
     echo "-1 $otime $time" > $FIFO
+
+    if [ "$message" != "none" ]; then
+        notify-send "pomo: $message"
+    fi
+
     play $ALARM &> /dev/null
 }
 
 pomo()
 {
-    local usage_str="usage: pomo [-t <number-of-minutes>] [-d --delete] [-p --pause] [-c --continue] [-r --restart]"
+    local usage_str="usage: pomo [-t <number-of-minutes>] [-d --delete] [-p --pause] [-c --continue] [-r --restart] [-m --message <message>]"
     [ ! -f "$FIFO" ] && touch "$FIFO"
 
-    case "$1" in
-        -t|--time)
-            shift
-            local mins="$1"
-            if [ -z $mins ]; then
-                echo "minutes input required!"
-            fi
+    while (($#)); do
+        case "$1" in
+            -t|--time)
+                shift
+                local mins="$1"
+                local message="none"
+                if [ -z $mins ]; then
+                    echo "minutes input required!"
+                fi
 
-            # check for existing pid
-            local oldpid=$(tail -n1 $FIFO | awk '{print $1;}')
-            if [ ! -z $oldpid ] && ((oldpid != -1)); then
-                kill $oldpid
-            fi
-            (nohup bash -c "__countdown $mins" &> /dev/null &)
-            ;;
-        -d|--delete)
-            local oldpid=$(tail -n1 $FIFO | awk '{print $1;}')
-            if [ ! -z $oldpid ] && ((oldpid != -1)); then
-                kill $oldpid
-            fi
-            ;;
-        -p|--pause)
-            local oldpid=$(tail -n1 $FIFO | awk '{print $1;}')
-            if [ ! -z $oldpid ] && ((oldpid != -1)); then
-                kill $oldpid
-            fi
-            ;;
-        -c|--continue)
-            local lastTime=$(tail -n1 $FIFO | awk '{print $3;}')
-            [ -z lastTime ] && exit 0
-            (nohup bash -c "__countdown $((lastTime / 60))" &> /dev/null &)
-            ;;
-        -r|--restart)
-            local oldpid=$(tail -n1 $FIFO | awk '{print $1;}')
-            if [ ! -z $oldpid ] && ((oldpid != -1)); then
-                kill $oldpid
-            fi
-            local lastTime=$(tail -n1 $FIFO | awk '{print $2;}')
-            [ -z lastTime ] && exit 0
-            (nohup bash -c "__countdown $((lastTime / 60))" &> /dev/null &)
-            ;;
-    esac
+                # check for existing pid
+                local oldpid=$(tail -n1 $FIFO | awk '{print $1;}')
+                if [ ! -z $oldpid ] && ((oldpid != -1)); then
+                    # todo, check that the pid doesn't have a new process name
+                    kill $oldpid 2>&1 | /dev/null
+                fi
+                local runTime="$mins"
+                shift
+                ;;
+            -d|--delete)
+                local oldpid=$(tail -n1 $FIFO | awk '{print $1;}')
+                if [ ! -z $oldpid ] && ((oldpid != -1)); then
+                    kill $oldpid
+                fi
+                return 0
+                ;;
+            -p|--pause)
+                local oldpid=$(tail -n1 $FIFO | awk '{print $1;}')
+                if [ ! -z $oldpid ] && ((oldpid != -1)); then
+                    kill $oldpid
+                fi
+                return 0
+                ;;
+            -c|--continue)
+                mins=$(tail -n1 $FIFO | awk '{print $3;}')
+                [ -z lastTime ] && exit 0
+                message=$(tail -n1 $FIFO | cut -d ' ' -f4-)
+                mins="$((lastTime / 60))"
+                shift
+                ;;
+            -r|--restart)
+                local oldpid=$(tail -n1 $FIFO | awk '{print $1;}')
+                if [ ! -z $oldpid ] && ((oldpid != -1)); then
+                    kill $oldpid
+                fi
+                local lastTime=$(tail -n1 $FIFO | awk '{print $2;}')
+                [ -z lastTime ] && exit 0
+                message=$(tail -n1 $FIFO | cut -d ' ' -f4-)
+                mins="$((lastTime / 60))"
+                shift
+                ;;
+            -m|--message)
+                shift
+                [ -z "$1" ] && {
+                    echo "no message supplied"
+                    return 1
+                }
+                local message="$1"
+                shift
+                ;;
+        esac
+    done
+
+    (nohup bash -c "__countdown $runTime \"$message\"" &> /dev/null &)
 }
 
 export -f pomo
